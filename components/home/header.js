d027614c031e05f66358013dbdcaa80e3b197691
JakariaJishan <jakariajishan987@gmail.com>
2022-05-15 00:45:03 +0600

    starting small

--- components/home/header.js
+++ components/home/header.js
@@ -1,9 +1,15 @@
 import React from "react";
-import sample from "../assets/bg-video.mp4";
 const Header = () => {
   return (
     <>
-      <video className="videoTag" src={sample} autoPlay loop muted></video>
+    <div className="grid grid-cols-2">
+      <div>
+        <h1 className="text-3xl">THE BEST FITNESS <br /> STUDIO IN TOWN</h1>
+        <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here, content here, making it look like readable English</p>
+        <button>JOIN US</button>
+      </div>
+      <div>we</div>
+    </div>
     </>
   );
 };
