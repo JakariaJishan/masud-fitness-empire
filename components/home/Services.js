a4915548907237c6440a80e881814a54f6c7413a
JakariaJishan <jakariajishan987@gmail.com>
2022-05-15 12:42:48 +0600

    baler background image

--- components/home/Services.js
+++ components/home/Services.js
@@ -1,39 +1,46 @@
-import { faWheatAwnCircleExclamation } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+    faBottleWater,
+    faDumbbell,
+    faSwatchbook
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 
 const Services = () => {
   const demoObj = [
     {
-      icon: faWheatAwnCircleExclamation,
+      icon: faSwatchbook,
       title: "PROGRESSION",
       body: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. ",
     },
     {
-      icon: "",
+      icon: faDumbbell,
       title: "WORKOUT",
       body: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
     },
     {
-      icon: "",
+      icon: faBottleWater,
       title: "NUTRITION",
       body: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. ",
     },
   ];
-  return <div className="grid grid-cols-3 gap-5 text-center w-3/4 m-auto text-white">
-
-      {
-          demoObj.map(items => {
-              return (
-                  <div key={items.title} className="bg-slate-700 p-10">
-                      <FontAwesomeIcon className="text-4xl" icon="fa-brands fa-twitter"/>
-                      <h1 className="text-3xl font-bold mt-10 mb-3">{items.title}</h1>
-                      <p>{items.body}</p>
-                  </div>
-              )
-          })
-      }
-  </div>;
+  return (
+    <div className="grid grid-cols-3 gap-5 text-center w-3/4 m-auto text-white">
+      {demoObj.map((items) => {
+        return (
+          <div
+            key={items.title}
+            className=""
+            styles={{ backgroundImage: "url('../assets/powerx-1.png')" }}
+          >
+            <FontAwesomeIcon className="text-4xl" icon={items.icon} />
+            <h1 className="text-3xl font-bold mt-10 mb-3">{items.title}</h1>
+            <p>{items.body}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Services;
--- components/home/header.js
+++ components/home/header.js
@@ -1,16 +1,30 @@
 import React from "react";
+import img from '../assets/powerx-1.png';
 const Header = () => {
+  const style = {
+    backgroundImage: `url('${img}')`
+  }
   return (
     <>
-    <div className="grid grid-cols-2">
-      <div>
-        <h1 className="text-3xl">THE BEST FITNESS <br /> STUDIO IN TOWN</h1>
-        <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here, content here, making it look like readable English</p>
-        <button>JOIN US</button>
+      <div
+        className="grid grid-cols-2 w-3/4 m-auto "
+        styles={style}
+      >
+        <div>
+          <h1 className="text-3xl">
+            THE BEST FITNESS <br /> STUDIO IN TOWN
+          </h1>
+          <p>
+            It is a long established fact that a reader will be distracted by
+            the readable content of a page when looking at its layout. The point
+            of using Lorem Ipsum is that it has a more-or-less normal
+            distribution of letters, as opposed to using Content here, content
+            here, making it look like readable English
+          </p>
+          <button>JOIN US</button>
+        </div>
+        <div>sadf</div>
       </div>
-      <div>sadf
-      </div>
-    </div>
     </>
   );
 };
