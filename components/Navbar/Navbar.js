13a7aa395345c67ba12b75c0a820d84d7c1f8e7d
JakariaJishan <jakariajishan987@gmail.com>
2022-05-16 22:11:41 +0600

    responsive pera dicche

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -1,9 +1,74 @@
+import { faHamburger } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 const Navbar = () => {
+  const [active, setActive] = useState(false);
+
+  const handleClick = () => {
+    setActive(!active);
+  };
   return (
     <>
-      <div className="flex justify-between items-center w-3/4 m-auto p-5">
+<nav className='flex items-center flex-wrap bg-black p-3 '>
+        <Link href='/'>
+          <a className='inline-flex items-center p-2 mr-4 '>
+            
+            <span className='text-xl text-white font-bold uppercase tracking-wide'>
+              masud fitness empire
+            </span>
+          </a>
+        </Link>
+        <button className=' inline-flex p-3 hover:bg-gray-900 rounded lg:hidden text-white ml-auto hover:text-white outline-none' onClick={handleClick}>
+          {
+            !active? <FontAwesomeIcon icon={faHamburger}/> : 'x'
+          }
+        </button>
+        <div  className={`${
+            active ? "" : "hidden"
+          }   w-full lg:inline-flex lg:flex-grow lg:w-auto`}>
+          <div className='lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start  flex flex-col lg:h-auto'>
+            <Link href='/'>
+              <a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-gray-600 hover:text-white '>
+                Home
+              </a>
+            </Link>
+            <Link href='/'>
+              <a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-gray-600 hover:text-white'>
+                Services
+              </a>
+            </Link>
+            <Link href='/'>
+              <a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-gray-600 hover:text-white'>
+                Our Classes
+              </a>
+            </Link>
+            <Link href='/'>
+              <a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-gray-600 hover:text-white'>
+                About Us
+              </a>
+            </Link>
+            <Link href='/'>
+              <a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-gray-600 hover:text-white'>
+                Block
+              </a>
+            </Link>
+            <Link href='/'>
+              <a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-gray-600 hover:text-white'>
+                Pricing
+              </a>
+            </Link>
+            <Link href='/'>
+              <a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-gray-600 hover:text-white'>
+                Contact Us
+              </a>
+            </Link>
+          </div>
+        </div>
+      </nav>
+
+    {/* row nav bar */}
+      {/* <div className="flex justify-between items-center w-3/4 m-auto p-5">
         <div>logo</div>
         <div>
           <ul className="flex justify-center items-center ">
@@ -44,7 +109,7 @@ const Navbar = () => {
             </li>
           </ul>
         </div>
-      </div>
+      </div> */}
     </>
   );
 };
--- components/home/header.js
+++ components/home/header.js
@@ -1,25 +1,30 @@
 import React from "react";
 const Header = () => {
-  
   return (
     <>
-      <div
-        className="grid grid-cols-2 w-3/4 m-auto "
-      >
-        <div>
-          <h1 className="text-3xl">
-            THE BEST FITNESS <br /> STUDIO IN TOWN
-          </h1>
-          <p>
-            It is a long established fact that a reader will be distracted by
-            the readable content of a page when looking at its layout. The point
-            of using Lorem Ipsum is that it has a more-or-less normal
-            distribution of letters, as opposed to using Content here, content
-            here, making it look like readable English
-          </p>
-          <button>JOIN US</button>
+      <div className="relative ">
+        <video
+          muted
+          autoPlay={"autoplay"}
+          preload="auto"
+          loop
+          src={"/assets/gym-cinematic-2.mp4"}
+          className="w-screen h-full"
+        />
+        <div className="grid lg:grid-cols-2  absolute top-0 left-0 text-white">
+          <div className="m-5">
+            <h1 className="text-xl  font-extrabold">
+              THE BEST FITNESS <br /> STUDIO IN TOWN
+            </h1>
+            <p className="text-xs my-5">
+              It is a long established fact that a reader will be distracted by
+              the readable content of a page when looking at its layout. The
+              point of using Lorem Ipsum is that it has a more-or-less normal
+            </p>
+            <button className="btn">JOIN US</button>
+          </div>
+          <div></div>
         </div>
-        <div>sadf</div>
       </div>
     </>
   );
