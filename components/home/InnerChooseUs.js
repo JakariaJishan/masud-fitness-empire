f09e784ea92f697487c884994b289ca8c9bb2ef1
JakariaJishan <jakariajishan987@gmail.com>
2022-05-18 15:07:35 +0600

    choose us page styling

--- components/home/InnerChooseUs.js
+++ components/home/InnerChooseUs.js
@@ -3,7 +3,7 @@ import React from 'react';
 const InnerChooseUs = (props) => {
     const {icon, body, title} = props.items;
     return (
-        <div className="shadow-lg border-4 border-white cursor-pointer p-10 hover:shadow-xl hover:border-4 hover:border-[#FCD842] ">
+        <div className="shadow-lg border-4 border-white cursor-pointer p-10 hover:shadow-2xl hover:border-4 hover:border-[#FCD842] ">
             <div >
                 <img className="mx-auto w-20" src={icon} alt="asdf" />
                 <h1 className="text-xl my-5 font-bold uppercase">{title}</h1>
